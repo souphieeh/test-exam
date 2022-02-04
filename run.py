a3db92fff0399d7a6641e797f941ba08aa04f0ce
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-02-04 10:34:48 +0100

    Added scraping website.

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 #import necessary modules
 from flask import Flask, render_template
+import json
 
 
 # set up flask webserver
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+
+def load_selectors():
+    with open("selectors.json", 'r') as f:
+        return json.load(f)
+
+
 # define route(s)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/css-selectors")
+def css_selectors():
+    return render_template("css-selectors.html", selectors=load_selectors())
 
 
 # starts the webserver
