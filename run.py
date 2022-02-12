0dcb2ec286a53900c2296f119fe3b26d7a14806f
olidec <oliver.decapitani@gmail.com>
2022-02-12 22:30:46 +0000

    video solutions

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #import necessary modules
 from flask import Flask, render_template
 import json
+from bs4 import BeautifulSoup
+import requests
 
 
 # set up flask webserver
@@ -15,25 +17,67 @@ def load_selectors():
 
 # webscraping function
 def my_scraper():
-    # YOUR CODE GOES HERE
-    pass
+    # get the URL in a useable form
+    url = "https://www.w3schools.com/cssref/css_selectors.asp"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # select your objects
+    table_rows = [elem for elem in soup.select('.ws-table-all tr')]
+    return table_rows
 
 
 # filter function
 def my_filter():
-    # YOUR CODE GOES HERE
-    pass
+    # define filter function
+    def filter_func(elem):
+        return True
+
+    # apply filter function
+    table_rows = list(filter(filter_func, table_rows))
+    return table_rows
 
 # output to json
-def write_json():
-    # YOUR CODE GOES HERE
-    pass
+def write_json(table_rows):
+    # create the structure for the json file
+    selectors = []
+
+    for row in table_rows:
+        cells = list(row.select('td'))
+        if cells:
+            entry = {
+                'selector': "placeholder text, which will be overwritten below",
+                'example': cells[1].text,
+                'description': cells[2].text,
+                
+            }
+            # we need the following code beacause not all entries in the first column are text - some are links (a-tag)
+            if cells[0].a:
+                entry['selector'] = cells[0].a.text
+            else:
+                entry['selector'] = cells[0].text
+
+            selectors.append(entry)
+            
+
+    with open("selectors.json", 'w') as f:
+        json.dump(selectors, f)
 
 
 # define route(s)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/scraping")
+def scraping():
+    return render_template("scraping.html")
+
+@app.route("/run_scraper")
+def run_scraper():
+    table_rows = my_scraper()
+    write_json(table_rows)
+    return render_template("run_scraper.html")
 
 
 @app.route("/css-selectors")
--- scrape.py
+++ scrape.py
@@ -3,44 +3,53 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+def my_scraper():
+    # get the URL in a useable form
+    url = "https://www.w3schools.com/cssref/css_selectors.asp"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# get the URL in a useable form
-url = "https://www.w3schools.com/cssref/css_selectors.asp"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# select your objects
-table_rows = [elem for elem in soup.select('.ws-table-all tr')]
-
-
-# define filter function
-def filter_func(elem):
-    return True
-
-# apply filter function
-table_rows = list(filter(filter_func, table_rows))
+    # select your objects
+    table_rows = [elem for elem in soup.select('.ws-table-all tr')]
+    return table_rows
 
 
-# create the structure for the json file
-selectors = []
+def my_filter():
+    # define filter function
+    def filter_func(elem):
+        return True
 
-for row in table_rows:
-    cells = list(row.select('td'))
-    if cells:
-        entry = {
-            'selector': "placeholder text, which will be overwritten below",
-            'example': cells[1].text,
-            'description': cells[2].text,
+    # apply filter function
+    table_rows = list(filter(filter_func, table_rows))
+    return table_rows
+
+
+
+def write_json():
+    # create the structure for the json file
+    selectors = []
+
+    for row in table_rows:
+        cells = list(row.select('td'))
+        if cells:
+            entry = {
+                'selector': "placeholder text, which will be overwritten below",
+                'example': cells[1].text,
+                'description': cells[2].text,
+                
+            }
+            # we need the following code beacause not all entries in the first column are text - some are links (a-tag)
+            if cells[0].a:
+                entry['selector'] = cells[0].a.text
+            else:
+                entry['selector'] = cells[0].text
+
+            selectors.append(entry)
             
-        }
-        # we need the following code beacause not all entries in the first column are text - some are links (a-tag)
-        if cells[0].a:
-            entry['selector'] = cells[0].a.text
-        else:
-            entry['selector'] = cells[0].text
 
-        selectors.append(entry)
-        
+    with open("selectors.json", 'w') as f:
+        json.dump(selectors, f)
 
-with open("selectors.json", 'w') as f:
-    json.dump(selectors, f)
+
+table_rows = my_scraper()
+write_json()
